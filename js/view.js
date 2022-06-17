e8fec171bff75f97c7fbae8279f8e0ac53f88f36
Michael-okike <michaelokike57@gmail.com>
2022-06-17 22:25:02 +0100

    sections

---
+++ js/view.js
@@ -0,0 +1,12 @@
+function renderView() {
+    let newObject = localStorage.getItem('viewedPost')
+    console.log(newObject);
+    let post = JSON.parse(newObject)
+    console.log(post)
+    
+    document.getElementById('post-id').innerHTML = post.id
+    document.getElementById('post-title').innerHTML = post.title
+    document.getElementById('post-body').innerHTML = post.body
+}
+
+renderView();
